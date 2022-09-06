8fbddc157208cfcdd24e8073715af2ae9f2907ef
Polyana-Rangel <polyanarangel17@gmail.com>
2022-09-06 13:53:36 -0300

    exercicio2

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,25 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+let idade = 24;
+let isMaiorIdade = true;
+let terminouEnsinoMedio = true;
+let isCursandoOutraFaculdade = true ;
 
-if(){
+if(idade>=18){
+    console.log("você é maior de idade!")
 
-}else{
+}else if (idade<18){
+    console.log("voce é menor de idade!")
+
+}
+
+if (terminouEnsinoMedio===true){
+        console.log("voce terminou o ensino médio")
 	
+}else if (terminouEnsinoMedio===false){
+    console.log("voce nao terminou o ensino medio!")
+
+}
+ if (isCursandoOutraFaculdade===true){
+    console.log("voce esta cursando faculdade!")
+}else{
+    console.log("voce nao esta cursando faculdade!")
 }
